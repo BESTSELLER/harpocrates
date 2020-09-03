0159da4b98f0fb0911ca991a08394769b5aa2977
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2020-09-03 17:01:18 +0200

    I hate this function

--- util/extractSecrets.go
+++ util/extractSecrets.go
@@ -4,14 +4,23 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/BESTSELLER/harpocrates/config"
 	"github.com/BESTSELLER/harpocrates/files"
-
 	"github.com/BESTSELLER/harpocrates/vault"
+	"github.com/mitchellh/mapstructure"
 )
+
+type MyResult map[string]interface{}
+
+func SecretWithKeys(a interface{}) ([]interface{}, bool) {
+	b, ok := a.([]interface{})
+	return b, ok
+}
 
 // ExtractSecrets will loop through al those damn interfaces
 func ExtractSecrets(input SecretJSON) map[string]interface{} {
-	result := make(map[string]interface{})
+	var result = make(MyResult)
+	var currentPrefix = config.Config.Prefix
 
 	for _, a := range input.Secrets {
 
@@ -23,51 +32,60 @@ func ExtractSecrets(input SecretJSON) map[string]interface{} {
 				os.Exit(1)
 			}
 
-			for c, d := range b {
-				e, ok := d.([]interface{})
-				if !ok {
-					fmt.Printf("Expected an array, got: '%s'\n", d)
-					os.Exit(1)
-				}
-				for _, f := range e {
+			aa := map[string]Secret{}
+			mapstructure.Decode(b, &aa)
+
+			for c, d := range aa {
+				setPrefix(d.Prefix, &currentPrefix)
+
+				for _, f := range d.Keys {
 
 					// If the key is just a secret path, then it will read that from Vault, otherwise:
 					if fmt.Sprintf("%T", f) != "string" {
-						g, ok := f.(map[string]interface{})
-						if !ok {
-							fmt.Printf("Expected map[string]interface{}, got: '%s'\n", a)
-							os.Exit(1)
-						}
-						for h, i := range g {
-							j, ok := i.(map[string]interface{})
-							if !ok {
-								fmt.Printf("Expected map[string]interface{}, got: '%s'\n", a)
-								os.Exit(1)
-							}
+						bb := map[string]SecretKeys{}
+						mapstructure.Decode(f, &bb)
 
-							some := j["saveAsFile"]
-							if some != nil {
+						for h, i := range bb {
+							setPrefix(i.Prefix, &currentPrefix)
+
+							if i.SaveAsFile != nil {
 								var secretValue = vault.ReadSecretKey(fmt.Sprintf("%s", c), h)
-								if some.(bool) {
+								if *i.SaveAsFile {
 									fmt.Println("Creating file...", h)
-									files.WriteFile(input.Output, h, secretValue)
+									files.WriteFile(input.Output, fmt.Sprintf("%s%s", currentPrefix, h), secretValue)
 								} else {
-									result[h] = secretValue
+									result.Add(h, secretValue, currentPrefix)
 								}
+							} else {
+								var secretValue = vault.ReadSecretKey(fmt.Sprintf("%s", c), h)
+								result.Add(h, secretValue, currentPrefix)
 							}
+							setPrefix(d.Prefix, &currentPrefix)
 						}
 					} else {
 						var secretValue = vault.ReadSecretKey(fmt.Sprintf("%s", c), fmt.Sprintf("%s", f))
-						result[fmt.Sprintf("%s", f)] = secretValue
+						result.Add(fmt.Sprintf("%s", f), secretValue, currentPrefix)
 					}
 				}
 			}
 		} else {
 			var secretValue = vault.ReadSecret(fmt.Sprintf("%s", a))
 			for aa, bb := range secretValue {
-				result[aa] = bb
+				result.Add(aa, fmt.Sprintf("%s", bb), currentPrefix)
 			}
 		}
 	}
 	return result
+}
+
+func setPrefix(potentialPrefix string, currentPrefix *string) {
+	if potentialPrefix != "" {
+		*currentPrefix = potentialPrefix
+	} else {
+		*currentPrefix = config.Config.Prefix
+	}
+}
+
+func (result MyResult) Add(key string, value string, prefix string) {
+	result[fmt.Sprintf("%s%s", prefix, key)] = value
 }
