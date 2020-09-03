fc36af17e988bb01ff92683ead06915bfb81031c
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2020-09-03 16:59:43 +0200

    extended the config spec

--- config/config.go
+++ config/config.go
@@ -1,29 +1,66 @@
 package config
 
 import (
-	"log"
+	"fmt"
+	"os"
+	"strings"
 
-	"github.com/kelseyhightower/envconfig"
+	"github.com/spf13/cobra"
 )
 
 // GlobalConfig defines the structure of the global configuration parameters
 type GlobalConfig struct {
-	VaultAddress string `required:"false" envconfig:"vault_address"`
-	ClusterName  string `required:"false" envconfig:"cluster_name"`
-	TokenPath    string `required:"false" envconfig:"token_path"`
-	SecretPrefix string `required:"false"`
+	VaultAddress string `required:"false"`
+	ClusterName  string `required:"false"`
+	TokenPath    string `required:"false"`
 	VaultToken   string `required:"false"`
+	Prefix       string `required:"false"`
+	Format       string `required:"false"`
+	Output       string `required:"false"`
 }
 
 // Config stores the Global Configuration.
 var Config GlobalConfig
 
-//LoadConfig Loads config from env
-func LoadConfig() {
+const (
+	notRequired = false
+	required    = true
+)
 
-	err := envconfig.Process("harpocrates", &Config)
-	if err != nil {
-		log.Fatal(err)
+// SyncEnvToFlags - We should be able to do this better!
+func SyncEnvToFlags(cmd *cobra.Command) {
+	if Config.VaultAddress == "" {
+		tryEnv("vault_address", &Config.VaultAddress, required, cmd)
 	}
+	if Config.ClusterName == "" {
+		tryEnv("cluster_name", &Config.ClusterName, required, cmd)
+	}
+	if Config.TokenPath == "" {
+		tryEnv("token_path", &Config.TokenPath, notRequired, cmd)
+	}
+	if Config.Prefix == "" {
+		tryEnv("prefix", &Config.Prefix, notRequired, cmd)
+	}
+	if Config.VaultToken == "" {
+		tryEnv("vault_token", &Config.VaultToken, notRequired, cmd)
+	}
+	if Config.Format == "" {
+		tryEnv("format", &Config.Format, notRequired, cmd)
+		if Config.Format == "" {
+			Config.Format = "env"
+		}
 
+	}
+}
+func tryEnv(env string, some *string, required bool, cmd *cobra.Command) {
+	envPrefix := "harpocrates_"
+
+	envVar, ok := os.LookupEnv(strings.ToUpper(fmt.Sprintf("%s%s", envPrefix, env)))
+	if ok == true && envVar != "" {
+		*some = envVar
+	} else {
+		if required {
+			cmd.MarkPersistentFlagRequired(env)
+		}
+	}
 }
