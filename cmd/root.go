5fe8fb496b7997bc6b0016a24b618abef83cd58d
Peter Brøndum <peter.brondum@bestseller.com>
2020-09-08 12:14:13 +0200

    short description on cluster-name

--- cmd/root.go
+++ cmd/root.go
@@ -78,7 +78,7 @@ func init() {
 	// Setup flags
 	rootCmd.PersistentFlags().StringVarP(&secretFile, "file", "f", "", "that contains the configuration to apply")
 	rootCmd.PersistentFlags().StringVar(&config.Config.VaultAddress, "vault-address", "", "url to vault e.g. https://vault.example.com")
-	rootCmd.PersistentFlags().StringVar(&config.Config.ClusterName, "cluster-name", "", "author name for copyright attribution")
+	rootCmd.PersistentFlags().StringVar(&config.Config.ClusterName, "cluster-name", "", "k8s auth method name, use when running as a k8s pod")
 	rootCmd.PersistentFlags().StringVar(&config.Config.TokenPath, "token-path", "", "/path/to/token/file")
 	rootCmd.PersistentFlags().StringVar(&config.Config.VaultToken, "vault-token", "", "vault token in clear text")
 
