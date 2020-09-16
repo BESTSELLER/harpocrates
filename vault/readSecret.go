3db802b82707a0e326e822d0b7c7341a2cf0dbaf
Peter Brøndum <peter.brondum@bestseller.com>
2020-09-16 15:05:44 +0200

    retry on missing data in path

--- vault/readSecret.go
+++ vault/readSecret.go
@@ -3,6 +3,7 @@ package vault
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 const keyNotFound = "The key '%s' was not found in the path '%s'\n"
@@ -20,6 +21,23 @@ func (client *API) ReadSecret(path string) (map[string]interface{}, error) {
 
 	if secretData == nil {
 		secretData = secretValues.Data
+	}
+
+	// Will append data to path and retry if "data" is empty and warnings is present
+	// if path contains data and warnings an error is returned
+	if fmt.Sprintf("%s", secretValues.Data) == fmt.Sprintf("%s", make(map[string]interface{})) {
+		if len(secretValues.Warnings) > 0 {
+			splitPath := strings.Split(path, "/")
+			if splitPath[1] == "data" {
+				return nil, fmt.Errorf("%s", strings.Join(secretValues.Warnings, ","))
+			}
+
+			appendData := []string{splitPath[0], "data"}
+			pathWithData := append(appendData, splitPath[1:]...)
+			return client.ReadSecret(strings.Join(pathWithData, "/"))
+
+		}
+		return nil, fmt.Errorf("No data recieved")
 	}
 
 	b, err := json.Marshal(secretData)
