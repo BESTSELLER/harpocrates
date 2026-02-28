59c04ed9b34783c8eeae9990e22af3ee94270c2b
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2026-02-28 17:24:38 +0100

    unh
    Signed-off-by: Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>

--- cmd/lsp.go
+++ cmd/lsp.go
@@ -1,12 +1,8 @@
 package cmd
 
 import (
-	"os"
-
-	"github.com/BESTSELLER/harpocrates/config"
 	"github.com/BESTSELLER/harpocrates/internal/lsp"
 	"github.com/BESTSELLER/harpocrates/vault"
-	"github.com/BESTSELLER/harpocrates/vault/gcp"
 	"github.com/spf13/cobra"
 )
 
@@ -28,20 +24,8 @@ func startLSP() {
 
 	// Try to get Vault initialized identical to `fetch`
 	loadLocalVaultToken()
-
-	if config.Config.GcpWorkloadID {
-		login, err := gcp.FetchVaultLogin(config.Config.VaultAddress, config.Config.AuthName)
-		if err == nil {
-			config.Config.VaultToken = login.Auth.ClientToken
-		}
-	}
-
-	var vaultClient *vault.API
-	// Try establishing the vault client if auth was loaded, if not gracefully fall back.
-	// We do not fail hard in LSP!
-	if os.Getenv("VAULT_TOKEN") != "" || os.Getenv("VAULT_ADDR") != "" {
-		vaultClient = vault.NewClient()
-	}
+	vault.Login()
+	vaultClient := vault.NewClient()
 
 	server := lsp.NewServer(vaultClient)
 	server.Start()
