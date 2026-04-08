d00bc38ed69a9b5f4a6242f0d51afd154584dbb2
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2026-04-08 10:08:46 +0200

    Validate Vault token and fail early (#543)
    Signed-off-by: Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>

--- vault/login.go
+++ vault/login.go
@@ -19,48 +19,55 @@ type JWTPayLoad struct {
 }
 
 // Login will exchange the JWT token for a Vault token
-func Login() {
-	// If a token exists, skip the login.
+func Login() error {
+	// If a token exists, validate it. If valid, skip the login.
 	if config.Config.VaultToken != "" {
-		return
+		client := NewClient()
+		_, err := client.Client.Auth().Token().LookupSelf()
+		if err == nil {
+			return nil
+		}
+		log.Warn().Err(err).Msg("Vault token is invalid or expired, falling back to other authentication methods")
+		config.Config.VaultToken = ""
 	}
 
 	if config.Config.GcpWorkloadID {
 		login, err := gcp.FetchVaultLogin(config.Config.VaultAddress, config.Config.AuthName)
 		if err != nil {
-			log.Fatal().Err(err).Msg("GcpWorkload Identity was enabled but auth failed")
+			return fmt.Errorf("GcpWorkload Identity was enabled but auth failed: %w", err)
 		}
 		config.Config.VaultToken = login.Auth.ClientToken
-		return
+		return nil
 	}
 
 	url := config.Config.VaultAddress + "/v1/auth/" + config.Config.AuthName + "/login"
 
 	payload, err := json.Marshal(JWTPayLoad{Jwt: token.Read(), Role: config.Config.RoleName})
 	if err != nil {
-		log.Fatal().Err(err).Msg("Unable to prepare jwt token")
+		return fmt.Errorf("unable to prepare jwt token: %w", err)
 	}
 
 	req, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(payload))
 	if err != nil {
-		log.Fatal().Err(err).Msg("Unable to create login request to Vault")
+		return fmt.Errorf("unable to create login request to Vault: %w", err)
 	}
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatal().Err(err).Msg("Unable to make login call to Vault")
+		return fmt.Errorf("unable to make login call to Vault: %w", err)
 	}
 	defer res.Body.Close() //nolint:errcheck // We don't care about errors from this
 
 	returnPayload := gcp.VaultLoginResult{}
 	err = json.NewDecoder(res.Body).Decode(&returnPayload)
 	if err != nil {
-		log.Fatal().Err(err).Msg("Unexpected response from Vault")
+		return fmt.Errorf("unexpected response from Vault: %w", err)
 	}
 
 	if len(returnPayload.Errors) != 0 {
-		log.Fatal().Err(fmt.Errorf("%s", returnPayload.Errors)).Msg("API call to Vault failed")
+		return fmt.Errorf("API call to Vault failed: %s", returnPayload.Errors)
 	}
 
 	config.Config.VaultToken = returnPayload.Auth.ClientToken
+	return nil
 }
